b5926b71f8f1f61cfa0075c11f993f764e52b19e
Mikhail <mishakeys20@gmail.com>
2025-02-26 21:30:49 -0500

    improve

--- Views/Settings/AccountView.xaml.cs
+++ Views/Settings/AccountView.xaml.cs
@@ -1,4 +1,9 @@
-﻿using Microsoft.UI.Xaml;
+﻿using System;
+using CroomsBellScheduleCS.Windows;
+using Microsoft.UI.Xaml;
+using Microsoft.UI.Xaml.Controls;
+using Windows.UI.Popups;
+using WinRT.Interop;
 
 namespace CroomsBellScheduleCS.Views.Settings;
 
@@ -14,5 +19,33 @@ public sealed partial class AccountView
     private void Page_Loaded(object sender, RoutedEventArgs e)
     {
         _initialized = true;
+    }
+
+    private async void Button_Click(object sender, RoutedEventArgs e)
+    {
+        MessageDialog dlg = new MessageDialog($"Do you accept the terms of service of croomstech.sched?")
+        {
+            Title = "Authenticate with croomstech.sched",
+
+        };
+
+        dlg.Commands.Add(new UICommand(
+            "Yes",
+            new UICommandInvokedHandler(this.CommandInvokedHandler_none)));
+        dlg.Commands.Add(new UICommand(
+            "No",
+            new UICommandInvokedHandler(this.CommandInvokedHandler_none)));
+
+        InitializeWithWindow.Initialize(dlg, WindowNative.GetWindowHandle(MainWindow.Instance));
+
+        if (await dlg.ShowAsync() == dlg.Commands[0])
+        {
+            // TODO
+        }
+    }
+
+    private void CommandInvokedHandler_none(IUICommand command)
+    {
+        
     }
 }
