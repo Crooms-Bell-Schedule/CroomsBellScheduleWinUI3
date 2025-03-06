d0aaa9a95bd6e77b688c2a3cf15926daec06779d
Mikhail Tyukin <mishakeys20@gmail.com>
2025-03-06 09:14:12 -0500

    fix incorrect for loop

--- Provider/APIProvider.cs
+++ Provider/APIProvider.cs
@@ -60,7 +60,7 @@ public class APIProvider : IBellScheduleProvider
         }
 
         // Add appropriate strings
-        for (int i = 0; i < 7; i++) strings.Add(i.ToString(), "Period " + i);
+        for (int i = 1; i < 8; i++) strings.Add(i.ToString(), "Period " + i);
 
         return new BellScheduleReader(bellSchedule, strings);
     }
