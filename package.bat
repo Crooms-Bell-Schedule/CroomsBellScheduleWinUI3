a430beced1bb0511306580fa43089707a51b9db1
Mikhail Tyukin <mishakeys20@gmail.com>
2025-08-22 09:53:27 -0400

    v4.3.7: update libraries

--- package.bat
+++ package.bat
@@ -1,4 +1,4 @@
 @echo off
 echo packaging...
-vpk pack -u CroomsBellSchedule --packTitle "Crooms Bell Schedule" --packAuthors MikhailSoftware -i Assets/croomsBellSchedule.ico -v 4.3.6 -p "bin\Release\win-x64\finalpublish" -e "Crooms Bell Schedule.exe" --splashImage Assets/splash.png
+vpk pack -u CroomsBellSchedule --packTitle "Crooms Bell Schedule" --packAuthors MikhailSoftware -i Assets/croomsBellSchedule.ico -v 4.3.7 -p "bin\Release\win-x64\finalpublish" -e "Crooms Bell Schedule.exe" --splashImage Assets/splash.png
 pause
