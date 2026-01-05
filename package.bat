dd0a8ac1325d298e980cadffff3dc4d9681f1948
Mikhail Tyukin <mishakeys20@gmail.com>
2026-01-05 17:17:25 -0500

    5.0.2

--- package.bat
+++ package.bat
@@ -1,4 +1,4 @@
 @echo off
 echo packaging...
-vpk pack -u CroomsBellSchedule --packTitle "Crooms Bell Schedule" --packAuthors MikhailSoftware -i CroomsBellSchedule/Assets/croomsBellSchedule.ico -v 5.0.1 -p "CroomsBellSchedule\bin\Release\win-x64\finalpublish" -e "Crooms Bell Schedule.exe" --splashImage CroomsBellSchedule/Assets/splash.png
+vpk pack -u CroomsBellSchedule --packTitle "Crooms Bell Schedule" --packAuthors MikhailSoftware -i CroomsBellSchedule/Assets/croomsBellSchedule.ico -v 5.0.2 -p "CroomsBellSchedule\bin\Release\win-x64\finalpublish" -e "Crooms Bell Schedule.exe" --splashImage CroomsBellSchedule/Assets/splash.png
 pause
